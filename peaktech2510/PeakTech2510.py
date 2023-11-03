722bfb5288521ca3805fd685556572dbfe560480
HVaadal <haavarmv@stud.ntnu.no>
2023-11-03 19:38:53 +0100

    Make data parsing more modular

--- peaktech2510/PeakTech2510.py
+++ peaktech2510/PeakTech2510.py
@@ -4,6 +4,88 @@ import argparse
 import io
 import ast
 
+# Constants
+DISPLAY_READING_LENGTH = 8
+DECIMAL_POINT_ALLOWED_VALUES = ['0', '1', '2', '3']
+POLARITY_ALLOWED_VALUES = ['Positive', 'Negative']
+DISPLAY_ALLOWED_VALUES = ['1', '2', '3', '4']
+
+class Annunciator:
+
+    annunciator_dict = {
+        "31": "HZ",
+        "34": "DCV",
+        "36": "DCA",
+        "38": "OHM",
+        "39": "KOHM",
+        "47": "WATT",
+        "48": "KWATT",
+        "50": "ACV",
+        "52": "ACA",
+        "54": "POWER_FACTOR",
+        "61": "HOUR",
+        "62": "MINUTE",
+        "63": "VA",
+        "64": "KVA",
+        "65": "KW/HR",
+        "F2": "W/HR"
+    }
+
+    def __init__(self, annunciator:str):
+        if annunciator in Annunciator.annunciator_dict:
+            self.annunciator = annunciator
+        else:
+            raise ValueError("Unknown or uninitialized annunciator", annunciator)
+
+    def get_str(self):
+        return Annunciator.annunciator_dict[self.annunciator]
+        
+
+class PeakTech2510OutputData:
+    
+    def __init__(self, display_reading, decimal_point, polarity, annunciator, display):
+        # Check data integrity
+        if type(display_reading) != str or len(display_reading) != DISPLAY_READING_LENGTH:
+            raise ValueError("Error when parsing display reading", display_reading)
+        if decimal_point not in DECIMAL_POINT_ALLOWED_VALUES:
+            raise ValueError("Got unallowed decimal_point value", decimal_point)
+        if polarity not in POLARITY_ALLOWED_VALUES:
+            raise ValueError("Got unallowed polarity value", polarity)
+        if (type(annunciator) != Annunciator):
+            raise ValueError("Annunciator is incorrect type", type(annunciator))
+        if display not in DISPLAY_ALLOWED_VALUES:
+            raise ValueError("Got unallowed display value", display)
+        
+        self.display_reading = display_reading
+        self.decimal_point = decimal_point
+        self.polarity = polarity
+        self.annunciator = annunciator
+        self.display = display
+
+    def get_display_reading(self):
+        if self.decimal_point == "0":
+            # No decimal point
+            return self.display_reading
+        else:
+            # Add decimal point
+            return self.display_reading[:-int(self.decimal_point)] + ',' + self.display_reading[-int(self.decimal_point):]
+
+    def get_display_reading_raw(self):
+        return self.display_reading
+
+    def get_decimal_point(self):
+        return self.decimal_point
+    
+    def get_polarity(self):
+        return self.polarity
+
+    def get_annunciator_str(self):
+        return self.annunciator.get_str()
+    
+    def get_display(self):
+        return self.display
+    
+
 class PeakTech2510:
 
     def __init__(self, serial_port, baudrate=9600, timeout=1.0):
@@ -14,7 +96,7 @@ class PeakTech2510:
         # self.input = [ast.literal_eval('b' + byte_string) for byte_string in data]
         self.input = data
 
-    def read_data(self):
+    def read_data(self) -> PeakTech2510OutputData:
         """Read data from the instrument and return parsed result."""
         data = []
         for i in range(16):
@@ -22,26 +104,46 @@ class PeakTech2510:
             print(byte)
             data.append(byte)
         if len(data) == 16:
-            return self.parse_data(data)
+            return self._parse_data(data)
         return None
 
     @staticmethod
-    def parse_data(data_stream):
+    def _parse_data(data_stream):
         """Parses the 16-digit data stream from the device."""
+        
+        # Reverse the data stream, so it fits with the description from the user manual
+        data_stream.reverse()
         parsed_data = {
-            'End_Word': data_stream[15],
-            'Display_reading': data_stream[14:6:-1].reverse(),
-            'Decimal_Point': data_stream[6],
-            'Polarity': 'Positive' if data_stream[5] == '0' else 'Negative',
-            'Annunciator_for_Display': data_stream[4:2:-1],
-            'LDC_Display': data_stream[2],
-            'D14': data_stream[1],
-            'Start_Word': data_stream[0]
+        'End_Word': data_stream[0],
+        'Display_reading': data_stream[1:9],
+        'Decimal_Point': data_stream[9],
+        'Polarity': 'Positive' if data_stream[10] == '0' else 'Negative',
+        'Annunciator_for_Display': data_stream[11:13],
+        'LDC_Display': data_stream[13],
+        'D14': data_stream[14],
+        'Start_Word': data_stream[15]
         }
-        reading = data_stream[14:6:-1]
-        reading.reverse()
-        parsed_data['Display_reading'] = reading
-        return parsed_data
+        # ... and reverse the data sequences that are longer than a single byte aswell
+        parsed_data['Display_reading'].reverse()
+        parsed_data['Annunciator_for_Display'].reverse()
+
+        # Create a data object
+        output_data = None
+        try:
+            output_data = PeakTech2510OutputData(display_reading="".join(parsed_data['Display_reading']), \
+                                                decimal_point=parsed_data['Decimal_Point'], \
+                                                polarity=parsed_data['Polarity'], \
+                                                annunciator=Annunciator("".join(parsed_data['Annunciator_for_Display'])), \
+                                                display=parsed_data['LDC_Display'])
+        except ValueError:
+            # Print an error, just return an empty reading with all zeroes
+            print("WARNING: Error when parsing output data")
+            return PeakTech2510OutputData(display_reading=" " * DISPLAY_READING_LENGTH, \
+                                                decimal_point='0', \
+                                                polarity='Positive', \
+                                                annunciator=Annunciator("31"), \
+                                                display="1")
+        return output_data
 
     def close(self):
         """Close the serial connection."""
