faae927eb85471c1a87f27eb9539230e6ec9cded
HVaadal <haavarmv@stud.ntnu.no>
2023-11-05 22:48:35 +0100

    Add plotting to test

--- examples/test_read_serial.py
+++ examples/test_read_serial.py
@@ -1,48 +1,90 @@
 import serial
 import csv
 import argparse
+import sys
+import os
 
-def parse_data(data_stream):
-    """
-    Parses the 16-digit data stream from the device and returns a dictionary of the parsed values.
-    """
-    parsed_data = {
-        'End_Word': data_stream[0],
-        'Display_reading': int(data_stream[1:9], 2),
-        'Decimal_Point': int(data_stream[9], 2),
-        'Polarity': 'Positive' if data_stream[10] == '0' else 'Negative',
-        'Annunciator_for_Display': int(data_stream[11:13], 2),
-        'LDC_Display': int(data_stream[13], 2),
-        'D14': data_stream[14],
-        'Start_Word': data_stream[15]
-    }
-    return parsed_data
+import matplotlib.pyplot as plt
+
+sys.path.append(os.getcwd()+"\..\peaktech2510")
+from PeakTech2510 import PeakTech2510
 
 def main(serial_port, baudrate, timeout, csv_filename):
-    # Open the serial connection
-    with serial.Serial(serial_port, baudrate=baudrate, timeout=timeout) as ser:
-        with open(csv_filename, 'w', newline='') as csv_file:
-            csv_writer = csv.DictWriter(csv_file, fieldnames=['End_Word', 'Display_reading', 'Decimal_Point', 'Polarity', 'Annunciator_for_Display', 'LDC_Display', 'D14', 'Start_Word'])
-            csv_writer.writeheader()
-            
-            while True:
-                try:
-                    # Read data from serial port
-                    data = ser.read(16)  # Read 16 bytes (assuming each digit corresponds to a byte)
-                    print("Read: ", data)
-                    if len(data) == 16:
-                        parsed_data = parse_data(data)
-                        csv_writer.writerow(parsed_data)
-                except KeyboardInterrupt:
-                    # Exit the loop if Ctrl+C is pressed
-                    break
+    instrument = PeakTech2510(serial_port, baudrate, timeout)
+    
+    # Clear the output csv files and write column headers
+    for i in range(1,5):
+        with open(csv_filename+str(i)+'.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=['Display_reading', 'Annunciator_for_Display', 'LDC_Display', 'Polarity'])
+            # csv_writer.writeheader()
+    
+    # Continuously read from instrument and update csv files with data
+    try:
+        while True:
+            data = instrument.read_data()
+            row = {}
+            row['Display_reading']= data.get_display_reading()
+            row['Annunciator_for_Display'] = data.get_annunciator_text_str()
+            row['LDC_Display'] = data.get_display()
+            row['Polarity'] = data.get_polarity()
+            if data:
+                with open(csv_filename+row['LDC_Display']+'.csv', 'a', newline='') as csv_file: 
+                    csv_writer = csv.DictWriter(csv_file, fieldnames=['Display_reading', 'Annunciator_for_Display', 'LDC_Display', 'Polarity'])
+                    csv_writer.writerow(row)
+    except KeyboardInterrupt:
+        pass
+    except IndexError:
+        # Reached the end of our input
+        # Plot data
+        plot_data()
+
+    instrument.close()
+
+def plot_data():
+    # File paths for your CSV files
+    csv_files = []
+    for i in range(1,5):
+        csv_files.append('output' + str(i) + '.csv')
+
+    # Function to read the first column from a CSV file
+    def read_first_column(filename):
+        with open(filename, 'r') as file:
+            reader = csv.reader(file)
+            return [float(row[0]) for row in reader if row]  # Extract the first column
+
+    # Read data from CSV files
+    datasets = []
+    for csv_file in csv_files:
+        datasets.append(read_first_column(csv_file))
+
+    # Create 2x2 subplots
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+
+    # Titles and labels (you can configure these)
+    titles = ['Power factor', 'Power', 'Voltage', 'Current']
+    x_labels = ['X1', 'X2', 'X3', 'X4']
+    y_labels = ['Power factor', 'Watts', 'Volts', 'Ampere']
+
+    # Plotting data in each subplot
+    # Assuming the data is sequential (1, 2, 3, ...)
+    for i, data in enumerate(datasets):
+        axs[i // 2, i % 2].plot(range(1, len(data) + 1), data)
+        axs[i // 2, i % 2].set_title(titles[i])
+        # axs[i // 2, i % 2].set_xlabel(x_labels[i])
+        axs[i // 2, i % 2].set_ylabel(y_labels[i])
+        axs[i // 2, i % 2].set_ylim(bottom=0)
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Read data from the RS-232 device and store it in a CSV file.")
     parser.add_argument('-p', '--port', type=str, required=True, help="Serial port name, e.g., COM3 or /dev/ttyS0.")
     parser.add_argument('-b', '--baudrate', type=int, default=9600, help="Baud rate. Default is 9600.")
     parser.add_argument('-t', '--timeout', type=float, default=1.0, help="Timeout for reading data in seconds. Default is 1 second.")
-    parser.add_argument('-f', '--file', type=str, default='output.csv', help="Output CSV filename. Default is 'output.csv'.")
+    parser.add_argument('-f', '--file', type=str, default='output', help="Output CSV filename. Default is 'output'.")
     
     args = parser.parse_args()
     
