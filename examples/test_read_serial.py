9c81b1e15f38e0a6676319f74ef9911a6e7b2096
HVaadal <haavarmv@stud.ntnu.no>
2023-11-14 22:17:36 +0100

    Improve plotting, file writing and data reset on button press

--- examples/test_read_serial.py
+++ examples/test_read_serial.py
@@ -1,5 +1,6 @@
 import csv
 import argparse
+import datetime
 import sys
 import os
 import queue
@@ -15,8 +16,10 @@ from PeakTech2510 import PeakTech2510
 
 GET_DATA_FROM_FILE = True
 TIMER_FONT = {'family':"Helvetica", 'size':40}
-PLOT_X_AXIS_LIMITS = {'left':0, 'right':300}
+PLOT_X_AXIS_SPAN = 30 # Number of seconds to show for each plot
 PLOT_Y_AXIS_LIMITS = [[0, 1.2], [200, 400], [210, 250], [0.5, 2]]
+NUM_PLOTS = 4
+DATA_HISTORY_DEFAULT_VALUE = [[[] for _ in range(2)] for _ in range(NUM_PLOTS)]
 
 running = False
 start_time = 0
@@ -32,75 +35,79 @@ def trigger_press():
         # Set initial time for counter
         start_time = time.time()
 
+        # Clear the data in a sensible way
+        data_history = DATA_HISTORY_DEFAULT_VALUE
+        data_queue = queue.Queue()
+
         # Start threads
-        threading.Thread(target=get_data, args=("COM3", 9600, 1, "output", GET_DATA_FROM_FILE), daemon=True).start()
+        threading.Thread(target=get_data, args=("COM3", 9600, 1, GET_DATA_FROM_FILE), daemon=True).start()
         threading.Thread(target=update_timer).start()
         
-        # Update plot is scheduled using tkinter.after(), but we need to kick it off by calling it the first time
+        # Update plot is scheduled using tkinter.after(), but we need to kick it off the first time
         update_plot()
 
         # Button is now stop button
         toggle_button['text'] = "STOP + SAVE"
+
     else:
-        # Clear the data in a sensible way
-        data_history = [[] for _ in range(num_plots)]
-        data_queue = queue.Queue()
+        # Store data and plot files
+        timestring = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+        # Clear the output csv files and write column headers
+        for i in range(4):
+            with open(timestring + '_' + titles[i] + '.csv', 'w', newline='') as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=['Time', titles[i]])
+                csv_writer.writeheader()
+                for j, data in enumerate(data_history[i][0]):
+                    if data is not None:
+                        csv_writer.writerow({'Time':data_history[i][1][j], titles[i]:data_history[i][0][j]})
 
         # Button is now start button
         toggle_button['text'] = "START"
 
 def update_timer():
     while running:
-        print(timer_label.keys())
         timer_label['text'] = f'Test time: {(time.time() - start_time):.2f}s'
 
-def get_data(serial_port, baudrate, timeout, csv_filename, input_from_file=False):
+def get_data(serial_port, baudrate, timeout, input_from_file=False):
     instrument = PeakTech2510(serial_port, baudrate, timeout, input_from_file)
-    # Clear the output csv files and write column headers
-    for i in range(1,5):
-        with open(csv_filename+str(i)+'.csv', 'w', newline='') as csv_file:
-            csv_writer = csv.DictWriter(csv_file, fieldnames=['Display_reading', 'Annunciator_for_Display', 'LCD_Display', 'Polarity'])
-            # csv_writer.writeheader()
-    
     # Continuously read from instrument and update csv files with data
     while running:
         # Insert a small delay when reading from file, to simulate more accurately the behaviour of actually connecting to an instrument
         if input_from_file:
-            time.sleep(0.01)
-        data = instrument.read_data()      
+            time.sleep(0.1)
+        data = instrument.read_data()    
         data_queue.put(data)
-        print("put data in q")
-        row = {}
-        row['Display_reading']= data.get_display_reading()
-        row['Annunciator_for_Display'] = data.get_annunciator_text_str()
-        row['LCD_Display'] = data.get_display()
-        row['Polarity'] = data.get_polarity()
-        if data:
-            with open(csv_filename+row['LCD_Display']+'.csv', 'a', newline='') as csv_file: 
-                csv_writer = csv.DictWriter(csv_file, fieldnames=['Display_reading', 'Annunciator_for_Display', 'LCD_Display', 'Polarity'])
-                csv_writer.writerow(row)
-
+        # print("Put data in queue", data)
     instrument.close()
 
 def update_plot():
+    # Pop all data put into the queue since last call of the function
     while not data_queue.empty():
         data=data_queue.get()
         print("got data from q", data.get_display_reading())
-        data_history[int(data.get_display())-1].append(float(data.get_display_reading()))
-    print(data_history[:][-10:])
+        data_history[int(data.get_display())-1][0].append(float(data.get_display_reading()))
+        data_history[int(data.get_display())-1][1].append(float(time.time()-start_time))
+    print(data_history[:][0][:-10])
+    
     # Update each subplot
     for i, ax in enumerate(axs.flatten()):
         ax.clear()
-        print("PLotting", i, " with data", data_history[i])
-        ax.plot(data_history[i], linewidth=2)
+        # print("PLotting", i, " with data length", len(data_history[i][0]), "and", len(data_history[i][1]))
+        ax.plot(data_history[i][1], data_history[i][0])
         ax.set_title(titles[i])
         ax.set_xlabel(x_labels[i])
         ax.set_ylabel(y_labels[i])
         ax.set_ylim(bottom=PLOT_Y_AXIS_LIMITS[i][0], top=PLOT_Y_AXIS_LIMITS[i][1])
-        ax.set_xlim(left=PLOT_X_AXIS_LIMITS['left'], right=PLOT_X_AXIS_LIMITS['right'])
-
+        if len(data_history[i][1]) >= 1:
+            left = 0 if (data_history[i][1][-1] < PLOT_X_AXIS_SPAN) else (data_history[i][1][-1] - PLOT_X_AXIS_SPAN)
+            right = PLOT_X_AXIS_SPAN if (data_history[i][1][-1] < PLOT_X_AXIS_SPAN) else data_history[i][1][-1]
+        else:
+            left = 0
+            right = PLOT_X_AXIS_SPAN
+        ax.set_xlim(left=left, right=right)
     canvas.draw()
 
+    # Schedule new update
     if running:
         root.after(500, update_plot)
 
@@ -116,15 +123,14 @@ if __name__ == "__main__":
     
     # GUI setup
     root = tk.Tk()
-    root.title("Hello world")
+    root.title("PeakTech2510")
     toggle_button = tk.Button(root, text="START", command=trigger_press, font=tk.font.Font(family=TIMER_FONT['family'], size=TIMER_FONT['size']))
     toggle_button.pack()
     timer_label = tk.Label(root, text="Test time 0.00s", font=tk.font.Font(family=TIMER_FONT['family'], size=TIMER_FONT['size']))
     timer_label.pack()
 
     # Plot setup
-    num_plots = 4
-    data_history = [[] for _ in range(num_plots)]
+    data_history = DATA_HISTORY_DEFAULT_VALUE
     titles = ['Power factor', 'Power', 'Voltage', 'Current']
     x_labels = ['X1', 'X2', 'X3', 'X4']
     y_labels = ['Power factor', 'Watts', 'Volts', 'Ampere']
